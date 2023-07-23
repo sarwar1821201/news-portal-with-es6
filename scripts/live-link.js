4f5ef15a8dc5ec8394b660b3a9fc7526c65b7e70
sarwar1821201 <sarwardipo1821201@gmail.com>
2023-07-23 15:20:02 +0600

    add live link site in the github

--- scripts/live-link.js
+++ scripts/live-link.js
@@ -1,1 +1,10 @@
 //  live link: https://eclectic-sorbet-6d233a.netlify.app/#
+
+/*
+const showTrending=()=>{
+        const trendingNews = fetchData.filter(singleData => singleData.others_info.is_trending === true);
+          const category_name = document.getElementById("category-name").innerText;
+        showAllNews(trendingNews, category_name);
+}
+
+*/
